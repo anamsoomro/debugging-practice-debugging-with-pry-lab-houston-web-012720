9979275bd1fd47ebda9aa58f9cc53d7793fcb989
anam soomro <anamsoomroed@gmail.com>
2020-01-14 04:53:19 +0000

    Automatically backed up by Learn

--- fix_using_tests/false_equivalency.rb
+++ fix_using_tests/false_equivalency.rb
@@ -12,9 +12,8 @@ def selection(num)
   if num = 1
     puts "YUM YUM MUNCH MUNCH MUNCH"
   elsif num = 2
-    puts HAM HAM HAM IN MY TUMMY"
+    puts "HAM HAM HAM IN MY TUMMY"
   end
-  
 end
 
 def runner
